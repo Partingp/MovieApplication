f129aa590fbef074b8279a1d052474ad9db289ff
Partingp <p.partington@lancaster.ac.uk>
2020-09-01 22:27:11 +0100

    Add login and register forms as well as script to make sure only one buttons appear at a time

---
+++ wwwroot/javascript/script.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+
+    //Hide buttons when a button is selected
+    $("#filtersButton").click(function () {
+        $("#login").collapse("hide");
+        $("#register").collapse("hide");
+    });
+
+    $("#loginButton").click(function () {
+        $("#filters").collapse("hide");
+        $("#register").collapse("hide");
+    });
+
+    $("#registerButton").click(function () {
+        $("#filters").collapse("hide");
+        $("#login").collapse("hide");
+    });
+
+
+    //Display/Hide password 
+    $("#showPassword").change(function () {
+        if ($("#showPassword").is(":checked")) {
+            $("#password").attr("type", "text");
+        }
+        else {
+            $("#password").attr("type", "password");
+        }
+    });
+ 
+});
+
+
